c8acea5ad5966ca9731cde91186edb32c6e51cbc
BasilYes <basilyevs@gmail.com>
2024-08-02 13:16:51 +0300

    use relative path

--- sdk_plugin.gd
+++ sdk_plugin.gd
@@ -6,9 +6,9 @@ var export_plugin: EditorExportPlugin = null
 
 
 func _enter_tree() -> void:
-	export_plugin = load("res://addons/yandex_sdk/sdk_export_plugin.gd").new()
+	export_plugin = preload("sdk_export_plugin.gd").new()
 	add_export_plugin(export_plugin)
-	add_autoload_singleton("YandexSDK", "res://addons/yandex_sdk/yandex_sdk.gd")
+	add_autoload_singleton("YandexSDK", "yandex_sdk.gd")
 
 func _exit_tree() -> void:
 	remove_export_plugin(export_plugin)
